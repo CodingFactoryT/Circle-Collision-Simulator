a75d416a372e63a5486aa1c557ef5f92b1b92e0c
CodingFactoryT <94389494+CodingFactoryT@users.noreply.github.com>
2022-07-11 18:38:55 +0200

    Added Collision support

--- Circle.java
+++ Circle.java
@@ -1,39 +1,69 @@
 import java.awt.Color;
+import java.util.ArrayList;
+
+import javax.imageio.plugins.tiff.ExifGPSTagSet;
+import javax.swing.RepaintManager;
+
+import MathLib.util.Vector2D;
 
 public class Circle {
 	final int DIAMETER = 30;
 	final int RADIUS = DIAMETER / 2;
-	int x;
-	int y;
-	int velocity_x = 2;
-	int velocity_y = 2;
+	Vector2D position;
+	final double mass = Math.PI*RADIUS*RADIUS;
+	Vector2D velocity;
 	Color color;
 	
-	public Circle(int x, int y, Color color) {
-		this.x = x;
-		this.y = y;
+	public Circle(Vector2D position, Vector2D velocity, Color color) {
+		this.position = position;
+		this.velocity = velocity;
 		this.color = color;
 	}
 	
-	public double distanceTo(Circle circle) {
-		int distance_x = Math.abs(this.x - circle.x);
-		int distance_y = Math.abs(this.y - circle.y);
-		return Math.sqrt(distance_x*distance_x + distance_y*distance_y);
+	public double squaredDistanceTo(Circle circle) {
+		double distance_x = Math.abs(this.position.x - circle.position.x);
+		double distance_y = Math.abs(this.position.y - circle.position.y);
+		return distance_x*distance_x + distance_y*distance_y;
 	}
 	
 	public boolean checkCollisionWith(Circle circle) {
-		return this.distanceTo(circle) < 2*RADIUS;
+		return this.squaredDistanceTo(circle) <= 4*RADIUS*RADIUS;
 	}
 	
 	public void handleBorderCollision() {
 		int bt = MainPanel.BORDER_THICKNESS;
-		if(x + RADIUS > MainFrame.WIDTH - bt || x - RADIUS < bt) {
-			velocity_x *= -1;
-			System.out.println(this.x);
+		
+		if(position.x + RADIUS >= MainFrame.WIDTH - bt || position.x - RADIUS <= bt) velocity = Vector2D.multiply(velocity, new Vector2D(-1, 1));
+		if(position.y + RADIUS >= MainFrame.HEIGHT - bt || position.y - RADIUS <= bt) velocity = Vector2D.multiply(velocity, new Vector2D(1, -1));
+	}
+
+	public static void handleCircleCollision(ArrayList<Circle> circles) {
+		Circle c1 = circles.get(0);
+		Circle c2 = circles.get(1);
+		
+		if(c1.checkCollisionWith(c2)) {	
+			System.out.println("Collision occured");
+			Vector2D c1Velocity = calculateNewVelocity(c1, c2);
+			Vector2D c2Velocity = calculateNewVelocity(c2, c1);
+			c1.velocity = c1Velocity;
+			c2.velocity = c2Velocity;
+			System.out.println(c1Velocity.x + "|" + c1Velocity.y);
+			System.out.println(c2Velocity.x + "|" + c2Velocity.y);
 		}
-		if(y + RADIUS > MainFrame.HEIGHT - bt || y - RADIUS < bt) {
-			velocity_y *= -1;
-			System.out.println(this.y);
-		}
+	}
+	
+	public static Vector2D calculateNewVelocity(Circle c1, Circle c2) {
+		Vector2D p1 = c1.position;
+		Vector2D p2 = c2.position;
+		Vector2D v1 = c1.velocity;
+		Vector2D v2 = c2.velocity;
+
+		double first = 2*c2.mass / (c1.mass+c2.mass);
+		Vector2D second_upper = Vector2D.scalarProduct(Vector2D.subtract(v1, v2), Vector2D.subtract(p1, p2));
+		double second_lower = Math.pow(Vector2D.subtract(p1, p2).magnitude(), 2);
+		Vector2D second = second_upper.divide(second_lower);
+		Vector2D third = Vector2D.subtract(p1, p2);
+		
+		return Vector2D.subtract(v1, Vector2D.multiply(second.multiply(first), third));
 	}
 }
--- MainPanel.java
+++ MainPanel.java
@@ -1,21 +1,33 @@
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import javax.swing.plaf.ColorUIResource;
+
+import MathLib.util.Vector2D;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 public class MainPanel extends JPanel {
 	public static final int BORDER_THICKNESS = 10;
-	Circle circle = new Circle(30, 30, Color.red);
-	Timer timer = new Timer(1, new MovementHandler());
+	private ArrayList<Circle> circles = new ArrayList<>();
+	private int[] circleColors = new int[]{0x59D9B8, 0x5ECC6F, 0x7875B7};
+	
+	Circle circle1 = new Circle(new Vector2D(600, 200), new Vector2D(0,3),  new Color(circleColors[0]));
+	Circle circle2 = new Circle(new Vector2D(600, 700), new Vector2D(0,-3), new Color(circleColors[1]));
+
+	Timer timer = new Timer(16, new MovementHandler());
 
 	public MainPanel() {
 		this.setBounds(0,0,MainFrame.WIDTH, MainFrame.HEIGHT);
 		this.setBorder(BorderFactory.createLineBorder(new Color(0x487F66), BORDER_THICKNESS));
 		this.setBackground(new Color(0x28424C));
+		circles.add(circle1);
+		circles.add(circle2);
 		timer.start();
 	}
 	
@@ -23,17 +35,26 @@ public class MainPanel extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(circle.color);
-		g2d.fillOval(circle.x - circle.RADIUS,circle.y - circle.RADIUS,circle.DIAMETER,circle.DIAMETER);
+		
+		for(Circle circle : circles) {
+			g2d.setColor(circle.color);
+			g2d.fillOval((int) circle.position.x - circle.RADIUS, MainFrame.HEIGHT - (int) circle.position.y - circle.RADIUS,circle.DIAMETER,circle.DIAMETER);			
+		}
 	}
 	
 	public class MovementHandler implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			circle.x += circle.velocity_x;
-			circle.y += circle.velocity_y;
-			circle.handleBorderCollision();
+			for(Circle circle : circles) {
+				circle.position = Vector2D.add(circle.position, circle.velocity);
+			}				
+			
+			for(Circle circle : circles) {
+				circle.handleBorderCollision();
+			}
+			Circle.handleCircleCollision(circles);			
+			
 			repaint();
 		}
 		
